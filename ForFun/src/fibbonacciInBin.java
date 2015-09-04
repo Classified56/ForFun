6f9d0d554a2f4e6bc3f39235b6115d43b5386f74
Classified56 <Classified56@192.168.11.147>
2015-09-04 10:21:59 -0600

    Fibonacci in base 2, with input of how many numbers wanted/

--- ForFun/src/fibbonacciInBin.java
+++ ForFun/src/fibbonacciInBin.java
@@ -1,11 +1,19 @@
-
+import java.util.Scanner;
 public class fibbonacciInBin
 	{
-
+		static Scanner userInput = new Scanner(System.in);
 		public static void main(String[] args)
 			{
-				// TODO Auto-generated method stub
-
+				System.out.print("How many numbers would you like? ");
+				int x = userInput.nextInt(), a = 1, b = 1, sum = 0;
+				System.out.println("1 \n1");
+				for(int i = 2; i < x; i++)
+					{
+						sum = a + b;
+						b = a;
+						a = sum;
+						System.out.println(Integer.toBinaryString(sum));
+					}
 			}
 
 	}
