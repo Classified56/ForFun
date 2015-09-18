b79837ec0c5eeacfc109a34ae4819064ceb8f828
Classified56 <Classified56@192.168.12.202>
2015-09-18 11:53:07 -0600

    in bases less than 10

--- ForFun/src/fibbonacciInBin.java
+++ ForFun/src/fibbonacciInBin.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+
 public class fibbonacciInBin
 	{
 		static Scanner userInput = new Scanner(System.in);
@@ -6,14 +8,34 @@ public class fibbonacciInBin
 			{
 				System.out.print("How many numbers would you like? ");
 				int x = userInput.nextInt(), a = 1, b = 1, sum = 0;
+				System.out.print("What base would you like it in? ");
+				int base = userInput.nextInt();
 				System.out.println("1 \n1");
 				for(int i = 2; i < x; i++)
 					{
 						sum = a + b;
 						b = a;
 						a = sum;
-						System.out.println(Integer.toBinaryString(sum));
+						System.out.println(changeBase(sum, base));
 					}
+			}
+		public static int changeBase(int num, int  base)
+			{
+				ArrayList<Integer> numArray = new ArrayList<Integer>();
+				while(num > 0)
+					{
+						numArray.add(0, num % base);;
+						num /= base;
+					}
+				int newNum = 0;
+				for(int i = 0; i < numArray.size(); i++)
+					{
+						newNum += numArray.get(i);
+						if(i < numArray.size() - 1)
+							newNum *= 10;
+					}
+					
+				return newNum;
 			}
 
 	}
